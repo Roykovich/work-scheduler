bc369fa0d83b3c4f949eef37e77e1633611ea0b9
Ruben Ortiz <rubend.ortizl@gmail.com>
2022-12-16 00:49:04 -0300

    remove unnecesary function

--- calculations.py
+++ calculations.py
@@ -1,37 +1,4 @@
 import datetime 
-from gmail import parse_date
-
-# def create_calculations_v1(schedule):
-#   calculations = {
-#     "total-diurno": 0,
-#     "total-nocturno": 0,
-#     "hours": []
-#   }
-
-#   for day in schedule:
-#     parsed = parse_date(day)
-
-#     hours = {
-#       "diurno": 0,
-#       "nocturno": 0
-#     }
-
-#     parsed["start_date"] += datetime.timedelta(minutes=30)
-
-#     while parsed["start_date"] < parsed["end_date"]:
-
-#       if (6 <= parsed["start_date"].hour < 22):
-#         calculations["total-diurno"] += 1
-#         hours["diurno"] += 1
-#       else:
-#         calculations["total-nocturno"] += 1
-#         hours["nocturno"] += 1
-
-#       parsed["start_date"] += datetime.timedelta(hours=1)
-
-#     calculations["hours"].append(hours)
-  
-#   print(calculations)
 
 def create_calculations(date):
   if type(date) is list: return

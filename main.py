c95a4a1fb70b8806bc800526d7b35e66e5d8094c
Ruben Ortiz <rubend.ortizl@gmail.com>
2023-04-19 15:00:49 -0400

    add module that delets old entries in notion

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from googleapiclient.errors import HttpError
 from gmail import create_schedule, create_events_object
 from image import draw_table
 from notion import create_notion_object
+from deleteEmail import delete_old_entries
 
 if os.path.exists('credentials.yml'):
   with open("credentials.yml") as f:
@@ -72,33 +73,7 @@ def main():
 
       # check if the current day is greater than 15 to delete all the relations
       # in the pasts months, with that we can have a clear look of the next payment
-      
-      if len(sys.argv) > 1:
-        delete_arg = sys.argv[1]
-
-        if delete_arg == '-delete':
-          if  DATENOW.day >= 15:
-            print("Deleting old entries.")
-
-            database_response = requests.request("POST", DATABASE, headers=NOTION_HEADERS)
-            res_data = database_response.json()
-            results_array = res_data["results"]
-
-            for result in results_array:
-              entry_date = result["properties"]["Entrada"]["date"]["start"]
-              parsed_entrydate = datetime.datetime.fromisoformat(entry_date)
-              entry_id = result["id"]
-
-              if parsed_entrydate.strftime("%B") != DATENOW.strftime("%B"):
-                result["properties"]["Relation"]["relation"] = []
-                new_dump = json.dumps(result)
-                delete_response = requests.request("PATCH", f"{NOTION_URL}/{entry_id}", headers=NOTION_HEADERS, data=new_dump)
-
-            print("old entries deleted sucessfully.")
-          else:
-            print("Old dates have not been deleted.")
-        else:
-          print("wrong delete argument given. Try instead -delete")
+      delete_old_entries(DATABASE, NOTION_HEADERS, NOTION_URL)
 
       
       print("Starting Google calendar insertions...")
@@ -113,7 +88,7 @@ def main():
       print("Notion insertions added succesfully.")
 
     else:
-      print('Script not executed.')
+      delete_old_entries(DATABASE, NOTION_HEADERS, NOTION_URL)
       return
 
   except HttpError as error:
